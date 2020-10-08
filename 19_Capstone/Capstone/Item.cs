8165b3b34e4b5af634a106f65cd5e3fc7abf8490
Domenico Boyadjian <dboyagian.info@gmail.com>
2020-10-08 12:02:59 -0400

    Items Class setup

---
+++ 19_Capstone/Capstone/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    public class Item
+    {
+        public string SlotLocation { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+
+    }
+}
